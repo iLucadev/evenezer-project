aedfb9c8b05206da37b2ae0637abc6c588e519da
Lucas Iriarte <lucasdiriarte@gmail.com>
2021-02-17 14:51:33 -0300

    fix

--- src/containers/HomeContainer.jsx
+++ src/containers/HomeContainer.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const HomeContainer = () => {
-  return <main></main>;
+  return (
+    <main className="container mx-auto py-12 full-width">
+      <h1 className="flex justify-center font-semibold">
+        Bienvenido a la tienda online de Ebenezer
+      </h1>
+    </main>
+  );
 };
 
 export default HomeContainer;
